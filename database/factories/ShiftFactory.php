107275db6e15ab1cf31a72738f3d3321b325967c
WesleyVerbrugge <w.verbrugge@home.nl>
2023-01-10 19:49:19 +0100

    Update ShiftFactory.php

--- database/factories/ShiftFactory.php
+++ database/factories/ShiftFactory.php
@@ -26,7 +26,7 @@ class ShiftFactory extends Factory
         //Takes current timestap and adds 8 hours on top of it while formatting it for SQL use.
         $dateTimeFormattedB = Carbon::now()->addHours(8)->format("Y.m.d H:i:s");
         
-        //Returns a factory instance on which multiple inserts in the database can be done by the seeder. Depending on the factory call.
+        //Returns a factory instance on which multiple inserts in the database can be done by the seeder. The amount depending on the factory call.
         return [
             'shift_start_details' => $dateTimeFormattedA,
             'shift_end_details' => $dateTimeFormattedB,
