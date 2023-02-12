b349af1fb5c385b2b92518e1b50b224b40857b1a
WesleyVerbrugge <w.verbrugge@home.nl>
2023-02-12 16:12:40 +0100

    Update ShiftFactory.php

--- database/factories/ShiftFactory.php
+++ database/factories/ShiftFactory.php
@@ -21,10 +21,10 @@ class ShiftFactory extends Factory
         $faker = Faker\Factory::create();
         
         //Takes current timestamp and formats it for SQL use.
-        $dateTimeFormattedA = Carbon::now()->format("Y.m.d H:i:s");
+        $dateTimeFormattedA = Carbon::now()->addDay()->format("Y.m.d H:i:s");
         
         //Takes current timestap and adds 8 hours on top of it while formatting it for SQL use.
-        $dateTimeFormattedB = Carbon::now()->addHours(8)->format("Y.m.d H:i:s");
+        $dateTimeFormattedB = Carbon::now()->addDay()->addHours(8)->format("Y.m.d H:i:s");
         
         //Returns a factory instance on which multiple inserts in the database can be done by the seeder. The amount depending on the factory call.
         return [
