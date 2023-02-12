0fde07855cb8e59accbc4730130c1d0693fe76ab
WesleyVerbrugge <w.verbrugge@home.nl>
2023-02-12 15:57:16 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -32,12 +32,12 @@ class DatabaseSeeder extends Seeder
         
         $testInterface1 = New ConnectionInterface;
         $testInterface1->ip_adress = "145.87.242.169";
-        $testInterface1->token = "WNtrCh0if4bmd9YPUQsSUjoMUVI1Oc4ClnlipUXUU8KP7olU7kqMWDJ7G175ELfOSWSGOlf40FHv21JiU39VrU3RXrrKLX93AK5wybvtMrwUkEnXhmuBw7jzVOfw6Dix"
+        $testInterface1->token = "WNtrCh0if4bmd9YPUQsSUjoMUVI1Oc4ClnlipUXUU8KP7olU7kqMWDJ7G175ELfOSWSGOlf40FHv21JiU39VrU3RXrrKLX93AK5wybvtMrwUkEnXhmuBw7jzVOfw6Dix";
         $testInterface1->save();
             
         $testInterface2 = New ConnectionInterface;
         $testInterface2->ip_adress = "94.211.44.18";
-        $testInterface2->token = "ZoFPpWVvR17azWHY5wx6aqVWUE33iQM8BfxxUNQxRkl6LU1wrxVW1nFDGauhUgmmWobLNHcCUE4etCsm28z5x4PDZty0VuaoIg4GEtv5ItXRxl7s3ErbLaXVcmUu6R2Q"    
+        $testInterface2->token = "ZoFPpWVvR17azWHY5wx6aqVWUE33iQM8BfxxUNQxRkl6LU1wrxVW1nFDGauhUgmmWobLNHcCUE4etCsm28z5x4PDZty0VuaoIg4GEtv5ItXRxl7s3ErbLaXVcmUu6R2Q";    
         $testInterface2->save();
         
         //Generates rest of the test data.
