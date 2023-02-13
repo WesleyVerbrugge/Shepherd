a308c09ead47646e1825207181ebc2271299a7d6
WesleyVerbrugge <w.verbrugge@home.nl>
2023-02-13 15:52:15 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\ConnectionInterface;
+use App\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,13 +16,50 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // Creates the first user which for testing purposes is our super intendent.
+        // Creates the first few default roles (Employee, Team-lead, Developer).
+        
+        $defaultRole1 = new Role;
+        $defaultRole1->name = "Employee";
+        $defaultRole1->description = "Somebody working as a advisor for Vodafone Ziggo";
+        $defaultRole1->clearance_level = 1;
+        $defaultRole1->save();
+        
+        $defaultRole2 = new Role;
+        $defaultRole2->name = "Team-Lead";
+        $defaultRole2->description = "Somebody working as a team-lead for Vodafone Ziggo";
+        $defaultRole2->clearance_level = 2;
+        $defaultRole2->save();
+        
+        $defaultRole3 = new Role;
+        $defaultRole3->name = "Developer";
+        $defaultRole3->description = "Someone working on improving the CBuddy system'";
+        $defaultRole3->clearance_level = 3;
+        $defaultRole3->save();
+        
+        // Creates the first test user which for testing purposes is our super intendent and has the team-lead role.
         $firstUser = new User;
         $firstUser->full_name = "Harry Visser";
         $firstUser->e_mail = "harry.visser@vodafoneziggo.com";
         $firstUser->username = "Harry.V";
         $firstUser->save();
-
+        $firstUser->roles()->attach($defaultRole2->id);
+        
+        // Creates 3 shifts for the second test user.
+        
+        $secondUserShifts = \App\Models\Shift::factory()->count(3)
+        
+        // Creates the second test user with developer and employee role.  
+            
+        $secondUser = new User;
+        $secondUser->full_name = "Wesley Verbrugge";
+        $secondUser->e_mail = "wesley.verbrugge@vodafoneziggo.com";
+        $secondUser->username = "R3solution";
+        $secondUser->save();
+        foreach($secondUserShifts as $shift) {
+        $secondUser->shifts()->attach($shift->id);
+        }
+        $secondUser->roles()->attach($defaultRole3->id, $defaultRole1->id);
+        
         // Creates first few connection interfaces for several developers and front-end that is authenticatable.
 
         $firstInterface = New ConnectionInterface;
@@ -39,12 +77,25 @@ class DatabaseSeeder extends Seeder
         $testInterface2->token = "ZoFPpWVvR17azWHY5wx6aqVWUE33iQM8BfxxUNQxRkl6LU1wrxVW1nFDGauhUgmmWobLNHcCUE4etCsm28z5x4PDZty0VuaoIg4GEtv5ItXRxl7s3ErbLaXVcmUu6R2Q";    
         $testInterface2->save();
         
-        //Generates rest of the test data.
-
+        //Generates a few test users and a couple of shifts linked to them..
+        
         \App\Models\User::factory()
-        ->count(3)
+        ->count(1)
         ->hasAttached(\App\Models\Role::factory()->count(1))
         ->hasAttached(\App\Models\Shift::factory()->count(3))
         ->create();
+        
+        \App\Models\User::factory()
+        ->count(1)
+        ->hasAttached(\App\Models\Role::factory()->count(1))
+        ->hasAttached(\App\Models\Shift::factory()->count(3))
+        ->create();
+        
+        \App\Models\User::factory()
+        ->count(1)
+        ->hasAttached(\App\Models\Role::factory()->count(1))
+        ->hasAttached(\App\Models\Shift::factory()->count(3))
+        ->create();
+        
     }
 }
